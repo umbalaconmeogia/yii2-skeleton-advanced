67c1b6c42c9b63e7c0444dceab7bc52d164732b2
ThanhTT <umbalaconmeogia@gmail.com>
2021-06-26 05:45:30 +0900

    Update to Yii 2.0.42

--- src/app/environments/dev/frontend/config/main-local.php
+++ src/app/environments/dev/frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '7SaVFgc4_vzl1MEoNWTX4bXEqNO7WFyw',
+            'cookieValidationKey' => '',
         ],
         // 'environmentNotice' => [
         //     'class' => 'batsg\widgets\EnvironmentNotice',
